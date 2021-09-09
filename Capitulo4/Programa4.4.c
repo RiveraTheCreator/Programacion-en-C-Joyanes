e631f75fb7e380ac5902224cd046a2870506f674
RiveraTheCreator <iriverab1900@alumno.ipn.mx>
2021-09-08 21:14:04 -0500

    agregando programa4.4

---
+++ Capitulo4/Programa4.4.c
@@ -0,0 +1,18 @@
+/* 
+Program: Ejercicio 4.4 
+Developer: RiveraTheCreator
+Date:15/jul/2021
+Version:0
+Instrucciones: Cual es la salida?
+*/
+#include<stdio.h>
+int main()
+{
+	printf("* * * * * * * * * * * *\n");
+	printf("*                     *\n");
+	printf("*                     *\n");
+	printf("*                     *\n");
+	printf("*                     *\n");
+	printf("* * * * * * * * * * * *\n");
+	return 0;
+}
