b326cd7e2ca7503aeebf8688ad25715fcc83481a
RiveraTheCreator <iriverab1900@alumno.ipn.mx>
2021-09-09 23:39:45 -0500

    agregando programa 4.5.c

--- Capitulo4/Programa4.5.c
+++ Capitulo4/Programa4.5.c
@@ -3,14 +3,16 @@ Program: Ejercicio 4.4
 Developer: RiveraTheCreator
 Date:08/sep/2021
 Version:0
-Instrucciones: dibujar el rectangulo siguiente
+Instrucciones:leer e imprimir en el centro del rectangulo
 */
 #include<stdio.h>
+#include<string.h>
 int main()
-{
+{ char cad[] = "     ";
+	scanf("%5c",&cad);
 	printf("* * * * * * * * * * * *\n");
 	printf("*                     *\n");
-	printf("*                     *\n");
+	printf("*        %s        *\n",cad);
 	printf("*                     *\n");
 	printf("*                     *\n");
 	printf("* * * * * * * * * * * *\n");
