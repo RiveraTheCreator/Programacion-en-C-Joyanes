793ef3410dd48b7cbf7d90ff54fb0ac7175acd40
RiveraTheCreator <iriverab1900@alumno.ipn.mx>
2021-09-11 13:16:58 -0500

    Agregando Programa4.1.c

---
+++ Capitulo4/Programa4.1.c
@@ -0,0 +1,20 @@
+/* 
+Program: Ejercicio 4.4 
+Developer: RiveraTheCreator
+Date:08/sep/2021
+Version:0
+Instrucciones:leer 2 numeros de 3 digitos e imprimir el cociente, producto, y el resto
+*/
+#include <stdio.h>
+#include <stdlib.h>
+int main(int argc, char const *argv[])
+{
+	int n1=0,n2=0;
+	printf("introduce 2 numeros de tres digitos\n");
+	scanf("%3i",&n1);
+	scanf("%3i",&n2);
+	printf("Producto: %i\n",n1*n2);
+	printf("Resto: %i\n",n1%n2 );
+	printf("Cociente: %.3f\n",(float) n1/n2);
+	return 0;
+}
