4df2c49682c4eff6a277211f84c16bc8a0e18511
Alejandro Rivera <78719823+RiveraTheCreator@users.noreply.github.com>
2025-08-22 21:45:24 -0600

    Solución: Añade el programa Hola Mundo en C

---
+++ hola_mundo.c
@@ -0,0 +1,6 @@
+#include <stdio.h> // Incluimos la librería estándar de entrada/salida
+
+int main() { // La función principal donde comienza la ejecución del programa
+    printf("Hola Mundo!\n"); // Imprime "Hola Mundo!" en la consola, seguido de un salto de línea
+    return 0; // Indica que el programa finalizó con éxito
+}
